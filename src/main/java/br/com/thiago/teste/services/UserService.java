3b3c9d5ad82a77f63750b02572a5a81f2f422ad9
ThiagoDevSantos <thiagosantosilva.2021@gmail.com>
2022-04-24 01:55:45 -0300

    adicionando o endpoint create

--- src/main/java/br/com/thiago/teste/services/UserService.java
+++ src/main/java/br/com/thiago/teste/services/UserService.java
@@ -1,10 +1,12 @@
 package br.com.thiago.teste.services;
 
 import br.com.thiago.teste.domain.User;
+import br.com.thiago.teste.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById (Integer id);
     List <User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/thiago/teste/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/thiago/teste/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import br.com.thiago.teste.domain.dto.UserDTO;
 import br.com.thiago.teste.repositories.UserRepository;
 import br.com.thiago.teste.services.UserService;
 import br.com.thiago.teste.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
@@ -24,5 +28,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj,User.class));
     }
 }
