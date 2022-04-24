8a96a160b449a7b61242c27e93e956c62ef00ed0
ThiagoDevSantos <thiagosantosilva.2021@gmail.com>
2022-04-24 00:06:22 -0300

    adicionando o UserDTO e incrementando o model mapper

--- src/main/java/br/com/thiago/teste/resources/UserResource.java
+++ src/main/java/br/com/thiago/teste/resources/UserResource.java
@@ -1,7 +1,9 @@
 package br.com.thiago.teste.resources;
 
 import br.com.thiago.teste.domain.User;
+import br.com.thiago.teste.domain.dto.UserDTO;
 import br.com.thiago.teste.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
 
     }
 }
